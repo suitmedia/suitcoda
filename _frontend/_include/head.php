58e8923f52a6c4c85517be738d43c338cfd3e37f
setiawanjeje <setiawanjeje@gmail.com>
2015-09-23 11:19:22 +0700

    add colors, change into php file

---
+++ _frontend/_include/head.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <meta name="description" content="Baze is a front-end starter template">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    
+    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+
+    <title>Suitcoda</title>
+    
+    <link rel="stylesheet" href="assets/css/main.css">
+    <script src="assets/js/vendor/modernizr.min.js"></script>
+</head>
+<body>
+    <!--[if lt IE 8]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
--- _frontend/login.php
+++ _frontend/login.php
@@ -1,26 +1,4 @@
-<!doctype html>
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-    <meta name="description" content="Baze is a front-end starter template">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="format-detection" content="telephone=no">
-    
-    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="assets/img/favicon.png">
-
-    <title>Suitcoda</title>
-    
-    <link rel="stylesheet" href="assets/css/main.css">
-    <script src="assets/js/vendor/modernizr.min.js"></script>
-</head>
-<body>
-    <!--[if lt IE 8]>
-        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
+<?php include '_include/head.php'; ?>
 
     <div class="text-center block-up block">
         <div class="container">
@@ -46,7 +24,7 @@
                         <span>Remember me</span>
                     </label>
 
-                    <button class="btn btn--regular btn--orange float-right">Log in</button>
+                    <button class="btn btn--regular btn--primary float-right">Log in</button>
                 </form>
             </div>
 
@@ -57,7 +35,4 @@
     </div>
         
     
-    <script>window.myPrefix = '';</script>
-    <script src="assets/js/main.min.js"></script>
-</body>
-</html>
+<?php include '_include/script.php'; ?>
