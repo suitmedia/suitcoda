b04730fb53ae4e11b6908292eae315ff8fd067d4
setiawanjeje <setiawanjeje@gmail.com>
2015-09-29 09:49:55 +0700

    partisi markup header dan footer

---
+++ _frontend/_include/header.php
@@ -0,0 +1,45 @@
+    <header class="header">
+        <div class="container cf">
+            <img class="header-logo" src="assets/img/logo-header.png" alt="">
+            
+            <ul class="header-list cf">
+                <li>
+                    <a class="notif text-grey" href="#">
+                        <div class="notif__icon">
+                            <span class="fa fa-bell"></span>
+                        </div>
+                        <span class="notif__count">2</span>
+                    </a>
+                    <ul class="notif-list">
+                        <li>
+                            <a href="#">
+                                Testing #20 on Suitcoda Project has done tested.
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Testing #20 on Suitcoda Project has done tested.
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Testing #20 on Suitcoda Project has done tested.
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="user-avatar" href="#">
+                        <img src="assets/img/pp.png" alt="">
+                    </a>
+                    <ul class="user-menu">
+                        <li>
+                            <a href="#">
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </header>
--- _frontend/home.php
+++ _frontend/home.php
@@ -1,50 +1,6 @@
 <?php include '_include/head.php'; ?>
 
-    <header class="header">
-        <div class="container cf">
-            <img class="header-logo" src="assets/img/logo-header.png" alt="">
-            
-            <ul class="header-list cf">
-                <li>
-                    <a class="notif text-grey" href="#">
-                        <div class="notif__icon">
-                            <span class="fa fa-bell"></span>
-                        </div>
-                        <span class="notif__count">2</span>
-                    </a>
-                    <ul class="notif-list">
-                        <li>
-                            <a href="#">
-                                Testing #20 on Suitcoda Project has done tested.
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Testing #20 on Suitcoda Project has done tested.
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Testing #20 on Suitcoda Project has done tested.
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a class="user-avatar" href="#">
-                        <img src="assets/img/pp.png" alt="">
-                    </a>
-                    <ul class="user-menu">
-                        <li>
-                            <a href="#">
-                                Logout
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </header>
+    <?php include '_include/header.php'; ?>
 
     <div class="bg-grey">
         <div class="container">
@@ -77,11 +33,7 @@
         </div>
     </main>
 
-    <footer class="footer">
-        <div class="container">
-            <span>Copyright &copy; blablabla</span>
-        </div>
-    </footer>
+    <?php include '_include/footer.php'; ?>
         
     
 <?php include '_include/script.php'; ?>
