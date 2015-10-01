166b44d485c3e775465e723b0428c995579fac02
setiawanjeje <setiawanjeje@gmail.com>
2015-10-01 16:15:58 +0700

    return early implementation

--- _frontend/assets/js/main.min.js
+++ _frontend/assets/js/main.min.js
@@ -65,6 +65,8 @@
         projectTabNav: function () {
             var $tab = $('.project-nav__tab a');
 
+            if ( !$tab.length ) return;
+
             var $btnNewTesting = $('.btn-new-testing');
 
             $btnNewTesting.on('click', function() {
@@ -119,55 +121,70 @@
         },
 
         projectChart: function () {
-            $('.project-chart').highcharts({
-                title: {
-                    text: 'Project Overview of Suitcoda Testing',
-                    align: 'center'
-                },
-                xAxis: {
-                    categories: ['#1','#2','#3','#4','#5','#6','#7','#8','#9','#10','#11','#12']
-                },
-                yAxis: {
+            var $chart = $('.project-chart');
+
+            if ( !$chart.length ) return;
+
+            var init = function () {
+
+                $chart.highcharts({
                     title: {
-                        text: 'Percents (%)'
+                        text: 'Project Overview of Suitcoda Testing',
+                        align: 'center'
                     },
-                    plotLines: [{
-                        value: 0,
-                        width: 1,
-                        color: '#808080'
+                    xAxis: {
+                        categories: ['#1','#2','#3','#4','#5','#6','#7','#8','#9','#10','#11','#12']
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Percents (%)'
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                    },
+                    tooltip: {
+                        valueSuffix: '%'
+                    },
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom',
+                        borderWidth: 0
+                    },
+                    series: [{
+                        name: 'Overall',
+                        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                    }, {
+                        name: 'Performance',
+                        data: [0, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+                    }, {
+                        name: 'Code Quality',
+                        data: [0, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+                    }, {
+                        name: 'Security',
+                        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                    }, {
+                        name: 'SEO',
+                        data: [1.2, 2.3, 2.5, 5.4, 5.7, 5.9, 6.2, 7.1, 7.9, 9.1, 10.5, 12.3]
                     }]
-                },
-                tooltip: {
-                    valueSuffix: '%'
-                },
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom',
-                    borderWidth: 0
-                },
-                series: [{
-                    name: 'Overall',
-                    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                }, {
-                    name: 'Performance',
-                    data: [0, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-                }, {
-                    name: 'Code Quality',
-                    data: [0, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-                }, {
-                    name: 'Security',
-                    data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-                }, {
-                    name: 'SEO',
-                    data: [1.2, 2.3, 2.5, 5.4, 5.7, 5.9, 6.2, 7.1, 7.9, 9.1, 10.5, 12.3]
-                }]
+                });
+            };
+
+            Modernizr.load({
+                load    : assets._highcharts,
+                complete: init
             });
+
         },
 
         progressBar: function () {
             var $progress = $('.progress');
             var $progressBar = $('.progress__bar');
+
+            if ( !$progress.length ) return;
 
             for (var i = 0; i < $progress.length; i++) {
                 var $progressValue = $progress.eq(i).attr('data-percent');
@@ -176,11 +193,36 @@
         },
 
         syntaxHighlighter: function () {
-            SyntaxHighlighter.all();
+            var $issue = $('.issue__code');
+
+            if ( !$issue.length ) return;
+
+            var init = function () {
+                SyntaxHighlighter.all();
+            };
+
+            Modernizr.load([
+                {
+                    load    : assets._sh
+                },
+                {
+                    load    : assets._shCSS
+                },
+                {
+                    load    : assets._shJS
+                },
+                {
+                    load    : assets._shPHP,
+                    complete: init
+                }
+            ]);
+
         },
 
         showIssueCode: function () {
             var $trigger = $('.btn-show-code');
+
+            if ( !$trigger.length ) return; 
 
             $trigger.on('click', function() {
                 var $issueCode = $(this).next();
@@ -203,7 +245,20 @@
 
             if ( !$formToValidate.length ) return;
 
-            $formToValidate.bazeValidate();
+            var init = function () {
+                $formToValidate.bazeValidate();
+            };
+
+            Modernizr.load([
+                {
+                    load    : assets._sprintf,
+                },
+                {
+                    load    : assets._bazeValidate,
+                    complete: init
+                }
+            ]);
+
         }
 
     };
@@ -218,30 +273,9 @@
         ]);
     };
 
-    Modernizr.load([{
-        load    : assets._jquery_cdn
-    },
-    {
-        load    : assets._sh
-    },
-    {
-        load    : assets._shCSS
-    },
-    {
-        load    : assets._shJS
-    },
-    {
-        load    : assets._shPHP
-    },
-    {
-        load    : assets._sprintf
-    },
-    {
-        load    : assets._bazeValidate
-    },
-    {
-        load    : assets._highcharts,
+    Modernizr.load({
+        load    : assets._jquery_cdn,
         complete: checkJquery
-    }]);
+    });
 
 })( window, document );
