43c9892de7acddd599824134688bd77e60713e2c
setiawanjeje <setiawanjeje@gmail.com>
2015-10-01 12:43:55 +0700

    add forgot password page

---
+++ _frontend/forgot-password.php
@@ -0,0 +1,30 @@
+<?php include '_include/head.php'; ?>
+
+    <?php include '_include/header.php'; ?>
+
+    <main>
+        <div class="bg-grey">
+            <div class="text-center block-up block-double">
+                <div class="container">
+                    
+                    <h1 class="title block-double block-up">Forgot Password?</h1>
+
+                    <div class="box box-form box-form--wide block-quad">
+                        <form action="">
+                            <label for="forgot-email" class="sr-only">Email :</label>
+                            <input id="forgot-email" class="form-input form-input--block block" placeholder="Input your email here ...." type="text">
+                            
+                            <button class="btn btn--primary btn--regular">
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include '_include/footer.php'; ?>
+        
+    
+<?php include '_include/script.php'; ?>
