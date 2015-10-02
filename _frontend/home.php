eea873701ae99a50db24bed9408a8b2f77e1f04e
setiawanjeje <setiawanjeje@gmail.com>
2015-10-02 15:08:15 +0700

    add example of different status on project

--- _frontend/home.php
+++ _frontend/home.php
@@ -18,7 +18,20 @@
             <span class="empty-state">There is no project yet.</span>
 
             <div class="bzg">
-                <?php for ($i=0; $i < 10; $i++) { ?>
+                <?php for ($i=0; $i < 3; $i++) { ?>
+                    <div class="bzg_c block" data-col="s12,m6">
+                        <a class="box box--block cf" href="project.php">
+                            <div class="box__thumbnail">
+                                <span>Testing #12</span> <br>
+                                <b class="text-big">80%</b>
+                            </div>
+                            <div class="box__desc">
+                                <b>Project Name</b> <br>
+                                <span>Lastest update : </span> <time>23/09/2015 12.34</time> <br>
+                                <span>Status : </span> <b class="text-green">Completed</b>
+                            </div>
+                        </a>
+                    </div>
                     <div class="bzg_c block" data-col="s12,m6">
                         <a class="box box--block cf" href="project.php">
                             <div class="box__thumbnail">
@@ -29,6 +42,19 @@
                                 <b>Project Name</b> <br>
                                 <span>Lastest update : </span> <time>23/09/2015 12.34</time> <br>
                                 <span>Status : </span> <b class="text-orange">On Progress</b>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="bzg_c block" data-col="s12,m6">
+                        <a class="box box--block cf" href="project.php">
+                            <div class="box__thumbnail">
+                                <span>Testing #12</span> <br>
+                                <b class="text-big">80%</b>
+                            </div>
+                            <div class="box__desc">
+                                <b>Project Name</b> <br>
+                                <span>Lastest update : </span> <time>23/09/2015 12.34</time> <br>
+                                <span>Status : </span> <b class="text-red">Stopped</b>
                             </div>
                         </a>
                     </div>
