4207ef6679d918c5ab77bd5c575223276c192d44
setiawanjeje <setiawanjeje@gmail.com>
2015-10-06 14:07:03 +0700

    fix bg grey

--- _frontend/forgot-password.php
+++ _frontend/forgot-password.php
@@ -3,25 +3,23 @@
     <div class="site">
         <?php include '_include/header.php'; ?>
 
-        <main class="main site-content">
-            <div class="bg-grey">
-                <div class="text-center block-up block-double">
-                    <div class="container">
-                        
-                        <h1 class="title block-double block-up">Forgot Password?</h1>
+        <main class="main bg-grey site-content">
+            <div class="text-center block-up block-double">
+                <div class="container">
+                    
+                    <h1 class="title block-double block-up">Forgot Password?</h1>
 
-                        <div class="box box-form box-form--wide block-quad">
-                            <form action="" data-validate>
-                                <div class="block">
-                                    <label for="forgot-email" class="sr-only">Email :</label>
-                                    <input id="forgot-email" class="form-input form-input--block" placeholder="Input your email here ...." type="text" required>
-                                </div>
-                                
-                                <button class="btn btn--primary btn--regular">
-                                    Submit
-                                </button>
-                            </form>
-                        </div>
+                    <div class="box box-form box-form--wide">
+                        <form action="" data-validate>
+                            <div class="block">
+                                <label for="forgot-email" class="sr-only">Email :</label>
+                                <input id="forgot-email" class="form-input form-input--block" placeholder="Input your email here ...." type="text" required>
+                            </div>
+                            
+                            <button class="btn btn--primary btn--regular">
+                                Submit
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
