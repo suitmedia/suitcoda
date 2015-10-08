e8621750c156454ab796c8d38ca5f47a8213e634
setiawanjeje <setiawanjeje@gmail.com>
2015-10-08 16:04:17 +0700

    fix hanging background

--- _frontend/new-password.php
+++ _frontend/new-password.php
@@ -2,30 +2,28 @@
 
     <div class="site">
 
-        <main class="main site-content">
-            <div class="bg-grey">
-                <div class="text-center block-up block-double">
-                    <div class="container">
-                        
-                        <h1 class="title block-double block-up">Change your password</h1>
+        <main class="main bg-grey site-content">
+            <div class="text-center block-up block-double">
+                <div class="container">
+                    
+                    <h1 class="title block-double block-up">Change your password</h1>
 
-                        <div class="box box-form box-form--wide block-quad">
-                            <form action="" data-validate>
-                                <div class="block">
-                                    <label for="new-pass" class="sr-only">New Password :</label>
-                                    <input id="new-pass" class="form-input form-input--block" placeholder="New password" type="password" required>
-                                </div>
+                    <div class="box box-form box-form--wide block-quad">
+                        <form action="" data-validate>
+                            <div class="block">
+                                <label for="new-pass" class="sr-only">New Password :</label>
+                                <input id="new-pass" class="form-input form-input--block" placeholder="New password" type="password" required>
+                            </div>
 
-                                <div class="block">
-                                    <label for="new-pass-conf" class="sr-only">Confirm New Password :</label>
-                                    <input id="new-pass-conf" class="form-input form-input--block" placeholder="Confirm new password" type="password" required>
-                                </div>
-                                
-                                <button class="btn btn--primary btn--regular">
-                                    Submit
-                                </button>
-                            </form>
-                        </div>
+                            <div class="block">
+                                <label for="new-pass-conf" class="sr-only">Confirm New Password :</label>
+                                <input id="new-pass-conf" class="form-input form-input--block" placeholder="Confirm new password" type="password" required>
+                            </div>
+                            
+                            <button class="btn btn--primary btn--regular">
+                                Submit
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
