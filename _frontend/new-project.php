2981da30cd77a093df5509cd6d52df0195c814b9
setiawanjeje <setiawanjeje@gmail.com>
2015-10-13 10:11:54 +0700

    add label to form on new project page

--- _frontend/new-project.php
+++ _frontend/new-project.php
@@ -3,30 +3,42 @@
     <div class="site">
         <?php include '_include/header.php'; ?>
 
-        <main class="main site-content">
-            <div class="bg-grey">
-                <div class="text-center block-up block">
-                    <div class="container">
-                        
-                        <h1 class="title block block-up">Create New Project</h1>
+        <main class="main bg-grey site-content">
+            <div class="block-up block">
+                <div class="container">
+                    
+                    <h1 class="title text-center block block-up">Create New Project</h1>
 
-                        <div class="box box-form box-form--wide block">
-                            <form action="" data-validate>
-                                <div class="block">
-                                    <label for="project-name" class="sr-only">Project Name :</label>
-                                    <input id="project-name" class="form-input form-input--block" placeholder="Input Project Name Here ...." type="text" required>
+                    <div class="box box-form box-form--wide block">
+                        <form action="" data-validate>
+                            <div class="bzg block-half">
+                                <div class="bzg_c" data-col="s12,m4">
+                                    <label for="project-name" class="form-label">Project Name :</label>
                                 </div>
-
-                                <div class="block">
-                                    <label for="project-url" class="sr-only">Project URL :</label>
-                                    <input id="project-url" class="form-input form-input--block" placeholder="Input Project URL Here ...." type="text" required>
+                                <div class="bzg_c" data-col="s12,m8">
+                                    <input id="project-name" class="form-input form-input--block" type="text" required>
                                 </div>
+                            </div>
 
-                                <div class="block">
-                                    <label for="project-desc" class="sr-only">Project Description :</label>
-                                    <textarea id="project-desc" class="form-input form-input--block" placeholder="Input Project Description Here ...." rows="7" required></textarea>
+                            <div class="bzg block-half">
+                                <div class="bzg_c" data-col="s12,m4">
+                                    <label for="project-url" class="form-label">Project URL :</label>
                                 </div>
+                                <div class="bzg_c" data-col="s12,m8">
+                                    <input id="project-url" class="form-input form-input--block" type="text" required>
+                                </div>
+                            </div>
 
+                            <div class="bzg block-half">
+                                <div class="bzg_c" data-col="s12,m4">
+                                    <label for="project-desc" class="form-label">Project Description :</label>
+                                </div>
+                                <div class="bzg_c" data-col="s12,m8">
+                                    <textarea id="project-desc" class="form-input form-input--block" rows="7" required></textarea>
+                                </div>
+                            </div>
+                            
+                            <div class="text-center">
                                 <button class="btn btn--primary btn--regular">
                                     Save
                                 </button>
@@ -34,8 +46,8 @@
                                 <a class="btn btn--secondary btn--regular" href="home.php">
                                     Cancel
                                 </a>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
