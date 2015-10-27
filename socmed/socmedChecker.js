0844b34e650c7d36f0a4fd2d1878d913f4e163f8
setiawanjeje <setiawanjeje@gmail.com>
2015-10-27 11:43:29 +0700

    breakdown opengraph validation

--- socmed/socmedChecker.js
+++ socmed/socmedChecker.js
@@ -37,45 +37,178 @@ var resultSocmed = {
 // ------------------- checking meta tag -------------------
 var openPage = horseman.open( url );
 
-var socmedName      = ['Opengraph', 'Twitter Cards', 'Facebook Insight'];
-var socmedSelector  = ['meta[property*="og"]', 'meta[name*="twitter"]', 'meta[property*="fb"]'];
+// ----------------------- Open Graph -----------------------
+resultSocmed.checking.push({
+    socmedName  : 'Open Graph',
+    message     : []
+});
 
-socmedSelector.forEach(function (value, index) {
-    var isExist = horseman.exists(value);
-    
-    var getCode;
+var ogElem = 'meta[property*="og:"]';
 
-    if ( isExist ){
-        getCode = horseman.evaluate(function (selector) {
-            var result = document.querySelectorAll(selector);
-            var tempp = [];
+isOpengraph = horseman.exists( ogElem );
 
-            for (var i = 0; i < result.length; i++) {
-                tempp.push(result[i].outerHTML);
-            }
+if ( isOpengraph ){
 
-            return tempp;
-        }, value );
-    } else {
-        getCode = 'This meta tag does not exist';
+    // cek og necessary
+    var ogNecessaryElem = [
+        'meta[property="og:title"]',
+        'meta[property="og:type"]',
+        'meta[property="og:site_name"]',
+        'meta[property="og:url"]',
+        'meta[property="og:description"]',
+        'meta[property="og:locale"]'
+    ];
+
+    var ogNecessaryElemName = [
+        'og:title',
+        'og:type',
+        'og:site name',
+        'og:url',
+        'og:description',
+        'og:locale',
+    ];
+
+    var ogNecessaryTag = [
+        '<meta property="og:title" content="" />',
+        '<meta property="og:type" content="" />',
+        '<meta property="og:site_name" content="" />',
+        '<meta property="og:url" content="" />',
+        '<meta property="og:description" content="" />',
+        '<meta property="og:locale" content="" />'
+    ];
+
+    ogNecessaryElem.forEach(function (value, index) {
+        var isExist = horseman.exists(value);
+        var ogDesc;
+
+        if ( !isExist ){
+            // if does not exist,push to json
+            ogDesc = 'Open Graph with property ' + ogNecessaryElemName[index] + ' is not found. Please add this meta tag ' + 
+                    ogNecessaryTag[index] + ' to kept the standarization';
+
+            resultSocmed.checking[0].message.push({
+                error      : 'Error',
+                desc       : ogDesc
+            });
+        }
+    });
+
+    // cek og:type
+    var ogTypeElem      = 'meta[property="og:type"]';
+    var isExistOgType   = horseman.exists( ogTypeElem );
+
+    if ( isExistOgType ){
+        var ogTypeChoice    = ['website','article','video','music','books','profile'];
+        var getOgType       = horseman.attribute('meta[property="og:type"]','content');
+
+        if ( ogTypeChoice.indexOf(getOgType) < 0 ) {
+            ogDesc = 'Your Open Graph type [' + getOgType + '] not match with our standarization. [website, books, video, music, books, profile] Please use one of these type.';
+          
+            resultSocmed.checking[0].message.push({
+                error      : 'Error',
+                desc       : ogDesc
+            });
+        }
+
+        // if og:type = article
+        if ( getOgType === 'article' ){
+            var articleNecessaryName = [
+                'article:author',
+                'article:publisher',
+                'article:tag',
+                'article:published_time',
+                'article:modified_time',
+            ];
+
+            var articleNecessaryElem = [
+                'meta[property="article:author"]',
+                'meta[property="article:publisher"]',
+                'meta[property="article:tag"]',
+                'meta[property="article:published_time"]',
+                'meta[property="article:modified_time"]'
+            ];
+
+            var articleNecessaryTag = [
+                '<meta property="article:author" content="">',
+                '<meta property="article:publisher" content="">',
+                '<meta property="article:tag" content="">',
+                '<meta property="article:published_time" content="">',
+                '<meta property="article:modified_time" content="">',
+            ];
+
+            articleNecessaryElem.forEach(function (value, index) {
+                var isExist = horseman.exists(value);
+                var articleDesc;
+
+                if ( !isExist ) {
+                    articleDesc = 'Open Graph with property ' + articleNecessaryName[index] + ' is not found. Please add this meta tag ' + 
+                              articleNecessaryTag[index] + ' to kept the standarization';
+
+                    resultSocmed.checking[0].message.push({
+                        error       : 'Error',
+                        desc        : articleDesc
+                    });
+                }
+            });
+        }
     }
 
-    resultSocmed.checking.push({
-        socmedName : socmedName[index],
-        isExist    : isExist,
-        code       : getCode
+    // cek og image
+    var ogImgName       = 'Open Graph Image';
+    var ogImgElem       = 'meta[property="og:image"]';
+    var isExistOgImg    = horseman.exists( ogImgElem );
+    var ogImgDesc;
+
+    if ( isExistOgImg ){
+        var ogImgNecessaryName = [
+            'og:image:type',
+            'og:image:width',
+            'og:image:height'
+        ];
+
+        var ogImgNecessaryTag = [
+            '<meta property="og:image:type" content="">',
+            '<meta property="og:image:width" content="">',
+            '<meta property="og:image:height" content="">',
+        ];
+
+        var ogImgNecessaryElem = [
+            'meta[property="og:image:type"]',
+            'meta[property="og:image:width"]',
+            'meta[property="og:image:height"]'
+        ];
+
+        ogImgNecessaryElem.forEach(function (value, index) {
+            var isExist = horseman.exists( value );
+
+            if ( !isExist ){
+                ogImgDesc = 'Open Graph with property ' + ogImgNecessaryName[index] + ' is not found. Please add this meta tag ' + 
+                              ogImgNecessaryTag[index] + ' to kept the standarization';
+
+                resultSocmed.checking[0].message.push({
+                    error       : 'Error',
+                    desc        : ogImgDesc
+                });
+            } 
+        });
+    }
+} else {
+    resultSocmed.checking[0].message.push({
+        error       : 'Error',
+        desc        : 'Open Graph is not found'
     });
-});
+}
 
 // ------------------------ save to json file ------------------------
 var toJson = jsonPretty(resultSocmed);
+console.log(toJson);
 
-function saveReport () {
-    fs.writeFile(dest + 'resultSocmed.json', toJson, function (err) {
-        if (err) throw err;
-    }); 
-}
+// function saveReport () {
+//     fs.writeFile(dest + 'resultSocmed.json', toJson, function (err) {
+//         if (err) throw err;
+//     }); 
+// }
 
-saveReport();
+// saveReport();
 
 horseman.close();
