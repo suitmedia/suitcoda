010ab100a00c961bb28c72df65bbff7b200b9b74
setiawanjeje <setiawanjeje@gmail.com>
2015-10-30 10:41:12 +0700

    now can accept parameter to seo checking

--- seo/seoChecker.js
+++ seo/seoChecker.js
@@ -9,15 +9,24 @@ var Horseman    = require('node-horseman'),
 // --------------------------- get url ---------------------------
 program
 .version('0.0.1')
-.option('-url, --url [url]', 'input url')
-.option('-d, --destination [path]', 'input path to store the output')
+.option('-url, --url [url]', 'Input URL')
+.option('-d, --destination [path]', 'Input Path to Store The Output')
+.option('--title',      'Include Title Tag Checker')
+.option('--header',     'Include Header Tag Checker')
+.option('--footer',     'Include Footer Tag Checker')
+.option('--favicon',    'Include Favicon Checker')
+.option('--aria',       'Include ARIA Landmark Checker')
+.option('--noalt',      'Include No Alternate Text on Image Tag Checker')
+.option('--i18n',       'Include Internationalization (i18n) Checker')
+.option('--meta',       'Include Necessary Meta Tag Checker')
+.option('--heading',    'Include Heading Tag Checker')
 .parse(process.argv);
 
 var url     = program.url;
 var dest    = '';
     dest    = program.destination;
 
-if ( ! dest ) {
+if ( !dest ) {
     dest = '';
 }
 
@@ -41,173 +50,190 @@ var error   = 'Error',
 var openUrl = horseman.open(url);
 
 // ----------------------- Title tag checking -----------------------
-var countTitle = horseman.count( 'title' );
+if ( program.title ) {
+    var countTitle = horseman.count( 'title' );
 
-if ( countTitle === 0 ) {
-    errDesc = 'Title Tag is not found. Please add Title Tag to keep the standarization.';
+    if ( countTitle === 0 ) {
+        errDesc = 'Title Tag is not found. Please add Title Tag to keep the standarization.';
 
-    pushErrMsg(errDesc);
-} else if ( countTitle > 1 ) {
-    errDesc = 'We have found ' + countTitle + ' Title Tags. Title Tag should only be one.';
-    pushErrMsg(errDesc);
+        pushErrMsg(errDesc);
+    } else if ( countTitle > 1 ) {
+        errDesc = 'We have found ' + countTitle + ' Title Tags. Title Tag should only be one.';
+        pushErrMsg(errDesc);
+    }
 }
 
 // ----------------------- header tag checking -----------------------
-var countHeader = horseman.count( 'header' );
+if ( program.header ) {
+    var countHeader = horseman.count( 'header' );
 
-if ( countHeader === 0 ) {
-    errDesc = 'Header Tag is not found. Please add Header Tag to keep the standarization.';
-    pushErrMsg(errDesc);
+    if ( countHeader === 0 ) {
+        errDesc = 'Header Tag is not found. Please add Header Tag to keep the standarization.';
+        pushErrMsg(errDesc);
+    }
 }
 
 // ---------------------- footer tag checking ----------------------
-var countFooter = horseman.count( 'footer' );
+if ( program.footer ) {
+    var countFooter = horseman.count( 'footer' );
 
-if ( countFooter === 0 ) {
-    errDesc = 'Footer Tag is not found. Please add Footer Tag to keep the standarization.';
-    pushErrMsg(errDesc);
+    if ( countFooter === 0 ) {
+        errDesc = 'Footer Tag is not found. Please add Footer Tag to keep the standarization.';
+        pushErrMsg(errDesc);
+    }
 }
 
 // ----------------------------- favicon -----------------------------
+if ( program.favicon ) {
+    var countFavicon = horseman.count('link[rel*="icon"]');
 
-var countFavicon = horseman.count('link[rel*="icon"]');
-
-if ( countFavicon === 0 ) {
-    errDesc = 'Favicon is not found. Please add <link rel="icon" type="image/png" href="path/to/your/file"></link> to keep the standarization.';
-    pushErrMsg(errDesc);
+    if ( countFavicon === 0 ) {
+        errDesc = 'Favicon is not found. Please add <link rel="icon" type="image/png" href="path/to/your/file"></link> to keep the standarization.';
+        pushErrMsg(errDesc);
+    }
 }
 
 // -------------------------- ARIA Landmark --------------------------
-var arr_role        = ['banner', 'main', 'contentinfo', 'navigation', 'search'];
+if ( program.aria ) {
+    var arr_role        = ['banner', 'main', 'contentinfo', 'navigation', 'search'];
 
-for (var i = 0; i < arr_role.length; i++) {
-    var _current = '[role="' + arr_role[i] + '"]';
+    for (var i = 0; i < arr_role.length; i++) {
+        var _current = '[role="' + arr_role[i] + '"]';
 
-    var isExist = horseman.exists(_current);
+        var isExist = horseman.exists(_current);
 
-    if ( !isExist ) {
-        errDesc = 'This Aria Landmark : '+ arr_role[i] +' is not found. Please add ' + _current + ' to this page to keep the standarization.';
-        pushWarnMsg(errDesc);
+        if ( !isExist ) {
+            errDesc = 'This Aria Landmark : '+ arr_role[i] +' is not found. Please add ' + _current + ' to this page to keep the standarization.';
+            pushWarnMsg(errDesc);
+        }
     }
 }
 
 // --------------------------- image no alt ---------------------------
-var imgCount = horseman
-    .count('img');
+if ( program.noalt ) {
+    var imgCount = horseman
+        .count('img');
 
-var noAltTextCount = horseman
-    .count('img[alt=""]');
+    var noAltTextCount = horseman
+        .count('img[alt=""]');
 
-if ( noAltTextCount > 0 ) {
-    var noAltTextCode = horseman
-        .evaluate(function (selector) {
-            var selectorNoAlt = document.querySelectorAll('img[alt=""]');
+    if ( noAltTextCount > 0 ) {
+        var noAltTextCode = horseman
+            .evaluate(function (selector) {
+                var selectorNoAlt = document.querySelectorAll('img[alt=""]');
 
-            var tempp = [];
-            for (var i = 0; i < selectorNoAlt.length; i++) {
-                tempp.push(selectorNoAlt[i].outerHTML);
-            }
+                var tempp = [];
+                for (var i = 0; i < selectorNoAlt.length; i++) {
+                    tempp.push(selectorNoAlt[i].outerHTML);
+                }
 
-            return tempp;
+                return tempp;
 
-        }, 'img');
-    
-    for (var i = 0; i < noAltTextCode.length; i++) {
-        errDesc = 'This Image Tag ' + noAltTextCode[i] + ' has no Alternate Text. Please add the alternate text to keep the standarization.';
-        pushErrMsg(errDesc);
-    }
-}
-
-
-
-// ------------------------------ i18n ------------------------------
-var isLang = horseman.exists('html[lang]');
-
-if ( isLang ) {
-    var getLang = horseman.attribute('html','lang');
-    if ( getLang === "" ) {
-        errDesc = 'Please fill the Lang attribute to keep the standarization.';
-        pushErrMsg(errDesc);
-    }
-} else {
-    errDesc = 'Language attribute is not found. Please add <html lang=""> to keep the standarization.';
-    pushErrMsg(errDesc);
-}
-
-var isHttpEquiv = horseman.exists('meta[http-equiv]');
-var isCharset = horseman.exists('meta[charset]');
-var getHttpEquiv = horseman.attribute('meta[http-equiv]','http-equiv');
-var getCharset = horseman.attribute('meta[charset]','charset');
-
-if ( getHttpEquiv === "Content-Type" && ( getCharset === "utf-8" || getCharset === "UTF-8") ) {
-    errDesc = 'Please do not declare both of character encoding element (<meta charset="utf-8"/> and <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>).';
-    pushErrMsg(errDesc);
-} else {
-    if ( isCharset ) {
-        if ( getCharset === "" ) {
-            errDesc = 'Please fill the charset attribute on meta tag.';
-            pushErrMsg(errDesc);
-        } else if ( getCharset !== "utf-8" && getCharset !== "UTF-8" ) {
-            errDesc = 'Please only use "utf-8" for charset attribute on meta tag.';
-            pushErrMsg(errDesc);
-        } 
-    } else {
-        if ( getHttpEquiv === '' ) {
-            errDesc = 'Please fill the http-equiv attribute on meta tag.';
-            pushErrMsg(errDesc);
-        } else if ( getHttpEquiv !== 'Content-Type' ) {
-            errDesc = 'Http-equiv attribute on meta tag is wrong. Please change it to <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> to keep the standarization.';
-            pushErrMsg(errDesc);  
-        } else {
-            errDesc = 'Character encoding declaration is not found. Please add <meta charset="utf-8"/> or <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> to keep the standarization.';
+            }, 'img');
+        
+        for (var i = 0; i < noAltTextCode.length; i++) {
+            errDesc = 'This Image Tag ' + noAltTextCode[i] + ' has no Alternate Text. Please add the alternate text to keep the standarization.';
             pushErrMsg(errDesc);
         }
-    } 
+    }
+    
+}
 
+// ------------------------------ i18n ------------------------------
+if ( program.i18n ) {
+    
+    var isLang = horseman.exists('html[lang]');
+
+    if ( isLang ) {
+        var getLang = horseman.attribute('html','lang');
+        if ( getLang === "" ) {
+            errDesc = 'Please fill the Lang attribute to keep the standarization.';
+            pushErrMsg(errDesc);
+        }
+    } else {
+        errDesc = 'Language attribute is not found. Please add <html lang=""> to keep the standarization.';
+        pushErrMsg(errDesc);
+    }
+
+    var isHttpEquiv = horseman.exists('meta[http-equiv]');
+    var isCharset = horseman.exists('meta[charset]');
+    var getHttpEquiv = horseman.attribute('meta[http-equiv]','http-equiv');
+    var getCharset = horseman.attribute('meta[charset]','charset');
+
+    if ( getHttpEquiv === "Content-Type" && ( getCharset === "utf-8" || getCharset === "UTF-8") ) {
+        errDesc = 'Please do not declare both of character encoding element (<meta charset="utf-8"/> and <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>).';
+        pushErrMsg(errDesc);
+    } else {
+        if ( isCharset ) {
+            if ( getCharset === "" ) {
+                errDesc = 'Please fill the charset attribute on meta tag.';
+                pushErrMsg(errDesc);
+            } else if ( getCharset !== "utf-8" && getCharset !== "UTF-8" ) {
+                errDesc = 'Please only use "utf-8" for charset attribute on meta tag.';
+                pushErrMsg(errDesc);
+            } 
+        } else {
+            if ( getHttpEquiv === '' ) {
+                errDesc = 'Please fill the http-equiv attribute on meta tag.';
+                pushErrMsg(errDesc);
+            } else if ( getHttpEquiv !== 'Content-Type' ) {
+                errDesc = 'Http-equiv attribute on meta tag is wrong. Please change it to <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> to keep the standarization.';
+                pushErrMsg(errDesc);  
+            } else {
+                errDesc = 'Character encoding declaration is not found. Please add <meta charset="utf-8"/> or <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> to keep the standarization.';
+                pushErrMsg(errDesc);
+            }
+        } 
+
+    }
 }
 
 
 // ------------------------ meta tag checking ------------------------
+if ( program.meta ) {
+    var metaDesc   = 'meta[name="description"]',
+        metaVp     = 'meta[name="viewport"]';
 
-var metaDesc   = 'meta[name="description"]',
-    metaVp     = 'meta[name="viewport"]';
+    var isMetaDesc = horseman.exists(metaDesc);
+    if ( isMetaDesc ) {
+        var getMetaDesc = horseman.attribute('meta[name="description"]', 'content');
+        if ( getMetaDesc === "" ) {
+            errDesc = "Meta Description tag must be filled.";
+            pushErrMsg(errDesc);
+        }
+    } else {
+        errDesc = "Please add Meta Description tag to keep the standarization.";
+    }
 
-var isMetaDesc = horseman.exists(metaDesc);
-if ( isMetaDesc ) {
-    var getMetaDesc = horseman.attribute('meta[name="description"]', 'content');
-    if ( getMetaDesc === "" ) {
-        errDesc = "Meta Description tag must be filled.";
+    var isMetaVp = horseman.exists(metaVp);
+    if ( isMetaVp ) {
+        var getMetaVp = horseman.attribute('meta[name="viewport"]', 'content');
+        if ( getMetaVp === "" ) {
+            errDesc = "Meta Viewport tag must be filled";
+            pushErrMsg(errDesc);
+        }
+    } else {
+        errDesc = "Please add Meta Viewport tag to keep the standarization.";
         pushErrMsg(errDesc);
     }
-} else {
-    errDesc = "Please add Meta Description tag to keep the standarization.";
-}
-
-var isMetaVp = horseman.exists(metaVp);
-if ( isMetaVp ) {
-    var getMetaVp = horseman.attribute('meta[name="viewport"]', 'content');
-    if ( getMetaVp === "" ) {
-        errDesc = "Meta Viewport tag must be filled";
-        pushErrMsg(errDesc);
-    }
-} else {
-    errDesc = "Please add Meta Viewport tag to keep the standarization.";
-    pushErrMsg(errDesc);
+    
 }
 
 // ----------------------------- heading -----------------------------
-var isMainHeading = horseman.exists('h1');
+if ( program.heading ) {
+    var isMainHeading = horseman.exists('h1');
 
-if ( isMainHeading ) {
-    var countMainHeading = horseman.count('h1');
+    if ( isMainHeading ) {
+        var countMainHeading = horseman.count('h1');
 
-    if ( countMainHeading > 1 ) {
-        errDesc = 'Tag <h1> should only be one.';
-        pushWarnMsg(errDesc);
+        if ( countMainHeading > 1 ) {
+            errDesc = 'Tag <h1> should only be one.';
+            pushWarnMsg(errDesc);
+        }
+    } else {
+        errDesc = 'Tag <h1> is not found. Please add tag <h1></hi> to keep the standarization.'
+        pushErrMsg(errDesc);
     }
-} else {
-    errDesc = 'Tag <h1> is not found. Please add tag <h1></hi> to keep the standarization.'
-    pushErrMsg(errDesc);
 }
 
 // ------------------------ save to json file ------------------------
