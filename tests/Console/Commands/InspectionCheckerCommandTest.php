21d762e1b1285ac0614923286f858b8e6d6e0943
fznoviar <fznoviar@gmail.com>
2016-01-13 13:30:16 +0700

    Add new testcase

---
+++ tests/Console/Commands/InspectionCheckerCommandTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SuitTests\Console\Commands;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Mockery;
+use SuitTests\TestCase;
+use Suitcoda\Console\Commands\InspectionCheckerCommand;
+use Suitcoda\Model\Inspection;
+use Suitcoda\Model\JobInspect;
+use Suitcoda\Supports\CalculateScore;
+
+class InspectionCheckerCommandTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test handle InspectionCheckerCommand
+     *
+     * @return void
+     */
+    public function testHandle()
+    {
+        $inspectionFaker = factory(Inspection::class)->create();
+        factory(JobInspect::class)->create();
+
+        $inspection = Mockery::mock(Inspection::class);
+        $calc = Mockery::mock(CalculateScore::class);
+        $inspectionChecker = new InspectionCheckerCommand($inspection, $calc);
+
+        $inspection->shouldReceive('progress->get->first')->andReturn($inspectionFaker);
+        $calc->shouldReceive('calculate');
+        $inspection->shouldReceive('update')->andReturn(true);
+        
+        $inspectionChecker->handle();
+    }
+}
