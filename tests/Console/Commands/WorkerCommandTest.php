610473c20c57308dba1c0842d0fc80a0d0275189
fznoviar <fznoviar@gmail.com>
2016-01-15 19:16:28 +0700

    Remove testcase

--- tests/Console/Commands/WorkerCommandTest.php
+++ tests/Console/Commands/WorkerCommandTest.php
@@ -69,25 +69,4 @@ class WorkerCommandTest extends TestCase
 
         $worker->handle();
     }
-
-    public function testHandleResultTrue()
-    {
-        $jobFaker = factory(JobInspect::class)->create();
-
-        $job = Mockery::mock(JobInspect::class);
-        $reader = Mockery::mock(ResultReader::class);
-        $worker = new WorkerCommand($job, $reader);
-
-        $job->shouldReceive('getUnhandledJob->first')->andReturn($jobFaker);
-        $job->shouldReceive('update')->andReturn(true);
-        $reader->shouldReceive('setJob');
-        $reader->shouldReceive('run')->andReturn(true);
-
-        $worker->handle();
-
-        $this->seeInDatabase('job_inspects', [
-            'id' => $jobFaker->id,
-            'status' => -1
-        ]);
-    }
 }
